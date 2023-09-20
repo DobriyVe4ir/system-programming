0ccbbd9b47fcb2984a6ccde42fb935b8b2fd22f8
DobriyVe4ir <chit.graund@gmail.com>
2023-09-21 00:57:52 +0300

    1 строка

--- LR1/LR1.cpp
+++ LR1/LR1.cpp
@@ -8,6 +8,7 @@ int main()
     //CreateFile();
     
     std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
