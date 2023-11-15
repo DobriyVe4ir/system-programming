07d64b6c7aa5b239c178af7fd3080179396256ae
PC3 <chit.graund@gmail.com>
2023-11-15 10:08:30 +0200

    LR3 fix

--- LR3/LR3.cpp
+++ LR3/LR3.cpp
@@ -1,89 +1,89 @@
 ﻿#include "pch.h"
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+
 #define STR_EQUALS(str1) (strcmp(str1, argm) == 0)
 #define STRING_LEN FILENAME_MAX
 
-int main()
-{
-	printf_s("-zip [archive name] [<file names>]\n");
-	printf_s("-unzip [archive name]\n");
+void CreatePipes(HANDLE& hReadPipe, HANDLE& hWritePipe) {
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
 
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
+    CreatePipe(&hReadPipe, &hWritePipe, &saAttr, 0);
+    SetHandleInformation(hWritePipe, HANDLE_FLAG_INHERIT, 0);
+}
 
-	HANDLE pipeR, pipeW;
+int main() {
+    while (true) {
+        printf_s("-zip [archive name] [<file names>]\n");
+        printf_s("-unzip [archive name]\n");
 
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
-	si.dwFlags |= STARTF_USESTDHANDLES;
-	si.hStdOutput = pipeW;
-	si.hStdError = pipeW;
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
 
+        HANDLE pipeRead, pipeWrite;
 
-	PCHAR argm = new CHAR[20];
-	while (true)
-	{
-		std::cin >> argm;
-		if (STR_EQUALS("-zip")) {
-			//-zip archive1 pch.h 7za.dll
-			LPWSTR command = const_cast<LPWSTR>(TEXT("7za.exe a "));
-			LPWSTR wstr = new WCHAR[STRING_LEN];
-			std::wcin.ignore();
-			std::wcin.getline(wstr, STRING_LEN);
+        CreatePipes(pipeRead, pipeWrite);
 
-			LPWSTR fullCommand = new WCHAR[lstrlen(command) + lstrlen(wstr) + 1];
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+        si.dwFlags |= STARTF_USESTDHANDLES;
+        si.hStdOutput = pipeWrite;
+        si.hStdError = pipeWrite;
 
-			lstrcpy(fullCommand, command);
-			wcscat_s(fullCommand, lstrlen(command) + lstrlen(wstr) + 1, wstr);
-			
+        PCHAR argm = new CHAR[20];
 
+        std::cin >> argm;
+        LPWSTR command;
 
-			//7za.exe a + wstr
-			if (!CreateProcess(NULL,   // No module name (use command line)
-				fullCommand,      // Command line
-				NULL,           // Process handle not inheritable
-				NULL,           // Thread handle not inheritable
-				FALSE,          // Set handle inheritance to FALSE
-				0,              // No creation flags
-				NULL,           // Use parent's environment block
-				NULL,           // Use parent's starting directory 
-				&si,            // Pointer to STARTUPINFO structure
-				&pi)           // Pointer to PROCESS_INFORMATION structure
-				)
-			{
-				printf("CreateProcess failed (%d).\n", GetLastError());
-			}
-		}
-		else if (STR_EQUALS("-unzip")) {
-			LPWSTR command = const_cast<LPWSTR>(TEXT("7za.exe e "));
-			LPWSTR wstr = new WCHAR[STRING_LEN];
-			std::wcin.ignore();
-			std::wcin.getline(wstr, STRING_LEN);
+        if (STR_EQUALS("-zip")) {
+            command = const_cast<LPWSTR>(TEXT("7za.exe a "));
+        }
+        else if (STR_EQUALS("-unzip")) {
+            command = const_cast<LPWSTR>(TEXT("7za.exe e "));
+        }
+        else {
+            std::cerr << "Invalid command. Exiting." << std::endl;
+            break;
+        }
 
-			LPWSTR fullCommand = new WCHAR[lstrlen(command) + lstrlen(wstr) + 1];
+        LPWSTR wstr = new WCHAR[STRING_LEN];
 
-			lstrcpy(fullCommand, command);
-			wcscat_s(fullCommand, lstrlen(command) + lstrlen(wstr) + 1, wstr);
+        std::wcin.ignore();
+        std::wcin.getline(wstr, STRING_LEN);
 
-			//7za.exe e archive.7z
-			if (!CreateProcess(NULL,   // No module name (use command line)
-				fullCommand,      // Command line
-				NULL,           // Process handle not inheritable
-				NULL,           // Thread handle not inheritable
-				FALSE,          // Set handle inheritance to FALSE
-				0,              // No creation flags
-				NULL,           // Use parent's environment block
-				NULL,           // Use parent's starting directory 
-				&si,            // Pointer to STARTUPINFO structure
-				&pi)           // Pointer to PROCESS_INFORMATION structure
-				)
-			{
-				printf("CreateProcess failed (%d).\n", GetLastError());
-			}
-		}
-		WaitForSingleObject(pi.hProcess, INFINITE);
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
+        LPWSTR fullCommand = new WCHAR[lstrlen(command) + lstrlen(wstr) + 1];
 
-	}
+        lstrcpy(fullCommand, command);
+        wcscat_s(fullCommand, lstrlen(command) + lstrlen(wstr) + 1, wstr);
+
+        if (!CreateProcess(NULL, fullCommand, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi)) {
+            std::cerr << "CreateProcess failed (" << GetLastError() << ")." << std::endl;
+            break;
+        }
+
+        CloseHandle(pipeWrite);
+
+        char buffer[4096];
+        DWORD bytesRead;
+
+        while (ReadFile(pipeRead, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead != 0) {
+            std::cout.write(buffer, bytesRead);
+        }
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        CloseHandle(pipeRead);
+
+        delete[] argm;
+        delete[] wstr;
+        delete[] fullCommand;
+    }
+
+    return 0;
 }
